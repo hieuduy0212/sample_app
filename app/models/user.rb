ef03b515072ccb716bf8a6b78bd246259cb0f6af
hieunguyen <duyhieu0212@gmail.com>
2024-02-07 15:01:36 +0700

    chapter_9 advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,47 @@
 class User < ApplicationRecord
-  def self.digest string
-    cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create string, cost:
-  end
-
-  validates :email, format: {with: Regexp.new(Settings.validates.email.regex, "i")}
+  validates :email, format: {
+    with: Regexp.new(Settings.validates.email.regex, "i")
+  }
   validates :email, length: {maximum: Settings.validates.email.length}
   validates :email, presence: true
   validates :email, uniqueness: {case_sensitive: false}
   validates :name, presence: true
 
+  before_save :downcase_email
+
   has_secure_password
 
-  before_save :downcase_email
+  attr_accessor :remember_token
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost:
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_token
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_column :remember_digest, nil
+  end
 
   private
   def downcase_email
